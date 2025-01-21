52ad9b1d90859d5fdd7c6f07fffd6c089be9b9ef
rvk9741 <raghavendrakulkarni450@gmail.com>
2025-01-21 11:37:51 +0530

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-// import React from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 const App = () => {
-  return <h1 className="text-pink-400">App</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
